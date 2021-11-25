4e66480c4159964fdb9b808db566b65f539bd13a
L0WK3Y <lokarrcursed@protonmail.com>
2021-11-26 00:53:15 +0200

    Added a proper loop to the main.py app

--- main.py
+++ main.py
@@ -6,24 +6,28 @@ def show_divisors(divisor_list):
 
 cipher = FragmentationCipher()
 cipher.show_info()
+command = int(input("Enter a command:\n[1] Encrypt\n[2] Decrypt\n[3] Show info\n[0] Exit\n-> "))
 
-message = input("Enter your message:\n")
-message_length = len(message)
+while command != 0:
 
-divisors = divutil.find_divisors(message_length)
-print("Possible divisors: ")
-show_divisors(divisors)
+    message = input("Enter your message:\n")
+    message_length = len(message)
 
-fragments_num = int(input("Enter the number of fragments (must be one of the divisors):\n"))
-key_series = input("Enter a series of keys, one for each fragment:\n")
+    divisors = divutil.find_divisors(message_length)
+    print("Possible divisors: ")
+    show_divisors(divisors)
 
-command = int(input("Enter a command:\n[1] Encrypt\n[2] Decrypt\n[3] Show info\n-> "))
+    fragments_num = int(input("Enter the number of fragments (must be one of the divisors):\n"))
+    key_series = input("Enter a series of keys, one for each fragment:\n")
 
-if command == 1:
-    print(cipher.encode(message, fragments_num, key_series))
-elif command == 2:
-    print(cipher.decode(message, fragments_num, key_series))
-elif command == 3:
-    print((cipher.show_info()))
-else:
-    print("Invalid command!\n")
+    if command == 1:
+        print(cipher.encode(message, fragments_num, key_series))
+    elif command == 2:
+        print(cipher.decode(message, fragments_num, key_series))
+    elif command == 3:
+        print((cipher.show_info()))
+    else:
+        print("Invalid command!\n")
+
+    cipher.show_info()
+    command = int(input("Enter a command:\n[1] Encrypt\n[2] Decrypt\n[3] Show info\n-> "))
